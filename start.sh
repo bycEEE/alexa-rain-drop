2e5bf9d14b67d738d49d0216fd42b5398e97ea1b
Brian Choy <bycEEE@gmail.com>
2017-02-13 23:21:56 -0500

    Split cfn templates, update start script

--- start.sh
+++ start.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-BUCKET_NAME="alexa-rain-drop"
+set -e
+source config.env
 
+# Create setup stack
 aws cloudformation create-stack \
-	--stack-name AlexaRainDrop \
+	--stack-name AlexaRainDropSetup \
 	--template-body file://cfn-setup.yml \
 	--capabilities CAPABILITY_IAM \
 	--on-failure DO_NOTHING \
 	--parameters ParameterKey=S3BucketName,ParameterValue=$BUCKET_NAME
+
+# Wait until setup stack is created
+aws cloudformation wait stack-create-complete --stack-name AlexaRainDropSetup
+
+# Remove existing archives and create new ones
+rm -rf $RAIN_DROP_FILE $BAD_BOUJEE_FILE
+zip -9 $RAIN_DROP_FILE src/raindrop.py
+#zip -9 $BAD_BOUJEE_FILE src/badboujee.py
+
+# Upload files to s3
+aws s3 cp $RAIN_DROP_FILE s3://$BUCKET_NAME
+
+# Replace bucket names in yml template
+sed -i '' -e "s/BUCKET_NAME/$BUCKET_NAME/g" cfn-function.yml
+
+# Create function stack
+aws cloudformation create-stack \
+	--stack-name AlexaRainDropFunction \
+	--template-body file://cfn-function.yml \
+	--capabilities CAPABILITY_IAM \
+	--on-failure DO_NOTHING \
+
