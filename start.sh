2cca2800b8fef4a8686b318a08393969d20f800e
Brian Choy <bycEEE@gmail.com>
2017-02-13 22:12:05 -0500

    Add S3 bucket creation

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+BUCKET_NAME="alexa-rain-drop"
+
 aws cloudformation create-stack \
 	--stack-name AlexaRainDrop \
 	--template-body file://cfn-setup.yml \
 	--capabilities CAPABILITY_IAM \
-	--on-failure DO_NOTHING
+	--on-failure DO_NOTHING \
+	--parameters ParameterKey=S3BucketName,ParameterValue=${BUCKET_NAME}
